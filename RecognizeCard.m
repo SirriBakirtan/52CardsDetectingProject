59d73c55db5ef0e9685735343f7060fcb768d966
Sırrı Bakırtan <32266871+SirriBakirtan@users.noreply.github.com>
2020-05-09 19:37:37 +0300

    initial commit

---
+++ RecognizeCard.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = RecognizeCard(inputArg1,inputArg2)
+%RECOGNIZECARD Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
