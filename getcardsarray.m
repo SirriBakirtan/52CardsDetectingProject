6e12fd67794a400a0afd0f27afe91ba369141ba1
AnilYurtsever <ianilyurtsever@gmail.com>
2020-05-14 14:01:50 +0300

    card array creator function created

---
+++ getcardsarray.m
@@ -0,0 +1,18 @@
+function retval = getcardsarray (image, image_bw)
+  
+  image_stats = regionprops(image_bw, "boundingbox");
+  threshhold = uint16(length(image_bw(:, 1))) / 10;
+  cards = {};
+  for i = 1:length(image_stats)
+    if image_stats(i).BoundingBox(4) > threshhold
+      x = uint16(floor(image_stats(i).BoundingBox(2)));
+      y = uint16(floor(image_stats(i).BoundingBox(1)));
+      width = uint16(floor(image_stats(i).BoundingBox(4)));
+      height = uint16(floor(image_stats(i).BoundingBox(3)));
+      cards{end + 1} = image(x: x + width, y: y + height);
+    endif
+  endfor
+  
+  retval = cards;
+  
+endfunction
