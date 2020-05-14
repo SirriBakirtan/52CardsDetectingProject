a0d454ebc563d7da75717f6ab413b47aa48fc8ed
AnilYurtsever <ianilyurtsever@gmail.com>
2020-05-14 14:01:10 +0300

    binary black and white image converter function created

---
+++ convertbw.m
@@ -0,0 +1,13 @@
+function retval = convertbw (image)
+  
+  image_hsv = rgb2hsv(image);
+
+  image_hsv_v = image_hsv(:, :, 3);
+  image_hsv_s = image_hsv(:, :, 2);
+
+  image_hsv_v(image_hsv_v < 0.7 | image_hsv_s > 0.3) = 0;
+  image_hsv(:, :, 3) = image_hsv_v;
+  image_gray = rgb2gray(hsv2rgb(image_hsv));
+  retval = im2bw(image_gray, 0.5);
+
+endfunction
