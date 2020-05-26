fd7022f8532a80f58866daf0b7c9de2e33aca326
Sirri Bakirtan <sirribakirtan13@gmail.com>
2020-05-26 13:16:45 +0300

    WIP Convert to rectangle quadlitaral transformation

--- ConvertToRectangleCardImage.m
+++ ConvertToRectangleCardImage.m
@@ -1,16 +1,47 @@
-function [newGrayScaleImage] = ConvertToRectangleCardImage(Image)
+function [output] = ConvertToRectangleCardImage(Image)
+row = size(Image,1);
+col = size(Image,2);
 
-props = regionprops(Image, 'BoundingBox');
-boundingBox = props.BoundingBox;
-xLeft = boundingBox(1);
-yTop = boundingBox(2);
-width = boundingBox(3);
-height = boundingBox(4);
-boundingBox2 = [xLeft, yTop, width, height];
+if(row < col)
+    Image = imrotate(Image,90);
+end
 
-% Crop the image
-newGrayScaleImage = imcrop(Image, boundingBox2);
-imshow(newGrayScaleImage);
+targetImageData = Image;
+imshow(targetImageData);
 
+[X Y] = ginput(4);
+
+x=[1;210;210;1];
+y=[1;1;297;297];
+
+A=zeros(8,8);
+A(1,:)=[X(1),Y(1),1,0,0,0,-1*X(1)*x(1),-1*Y(1)*x(1)];
+A(2,:)=[0,0,0,X(1),Y(1),1,-1*X(1)*y(1),-1*Y(1)*y(1)];
+
+A(3,:)=[X(2),Y(2),1,0,0,0,-1*X(2)*x(2),-1*Y(2)*x(2)];
+A(4,:)=[0,0,0,X(2),Y(2),1,-1*X(2)*y(2),-1*Y(2)*y(2)];
+
+A(5,:)=[X(3),Y(3),1,0,0,0,-1*X(3)*x(3),-1*Y(3)*x(3)];
+A(6,:)=[0,0,0,X(3),Y(3),1,-1*X(3)*y(3),-1*Y(3)*y(3)];
+
+A(7,:)=[X(4),Y(4),1,0,0,0,-1*X(4)*x(4),-1*Y(4)*x(4)];
+A(8,:)=[0,0,0,X(4),Y(4),1,-1*X(4)*y(4),-1*Y(4)*y(4)];
+
+v=[x(1);y(1);x(2);y(2);x(3);y(3);x(4);y(4)];
+
+u=A\v;
+
+U=reshape([u;1],3,3)';
+
+T = projective2d(U');
+
+P2=imwarp(targetImageData,T);
+
+image_bw = ConvertImageToBlackAndWhite(P2);
+retval = GetCardsArray(P2, image_bw);
+retval{1}=imresize(retval{1},[451 650]);
+figure;
+imshow(retval{1});
+output =retval{1};
 end
 
